4da7393200091091aadd2d41fc2887ad1ce376e9
miya0001 <miya@wpist.me>
2014-12-31 04:26:12 +0900

    some fix

--- force-email-login.php
+++ force-email-login.php
@@ -1,61 +1,50 @@
 <?php
 /*
-Plugin Name: Force email login
+Plugin Name: Force Email Login
 Author: Takayuki Miyauchi
 Plugin URI: https://github.com/miya0001/force-email-login
 Description: Use email address for login to your WordPress.
-Version: 0.4.0
-Author URI: http://wpist.me/
+Version: 0.5.0
+Author URI: https://github.com/miya0001/
 */
 
-new Force_Email_Auth();
+$force_email_auth = new Force_Email_Auth();
+$force_email_auth->register();
+
 
 class Force_Email_Auth {
 
-function __construct()
-{
-    add_action('plugins_loaded', array(&$this, 'plugins_loaded'));
+	function register()
+	{
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+	}
+
+	public function plugins_loaded()
+	{
+		remove_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
+		add_filter( 'authenticate', array( $this, 'authenticate'), 20, 3 );
+	}
+
+	public function authenticate( $user, $username, $password )
+	{
+		if ( is_a( $user, 'WP_User' ) ) {
+			return $user;
+		}
+
+		if ( ! empty( $username ) && is_email( $username ) ) {
+			$user = get_user_by( 'email', $username );
+			if ( isset( $user, $user->user_login, $user->user_status ) ) {
+				if ( 0 === intval( $user->user_status ) ) {
+					$username = $user->user_login;
+					return wp_authenticate_username_password( null, $username, $password );
+				}
+			}
+		}
+
+		if ( ! empty( $username ) || ! empty( $password ) ) {
+			return false;
+		} else {
+			return wp_authenticate_username_password( null, "", "" );
+		}
+	}
 }
-
-public function plugins_loaded()
-{
-    if (get_transient('force_email_login_lockdown') && isset($_POST['log'])) {
-        wp_die('Please retry after a few seconds.');
-    }
-
-    remove_filter('authenticate', 'wp_authenticate_username_password', 20, 3);
-    add_filter('authenticate', array(&$this, 'authenticate'), 20, 3);
-    add_action('wp_login_failed', array(&$this, 'wp_login_failed'));
-}
-
-public function wp_login_failed()
-{
-    $lockdown = intval(apply_filters('force_email_login_lockdown', 10));
-    set_transient('force_email_login_lockdown', true, $lockdown);
-}
-
-public function authenticate( $user, $username, $password )
-{
-    if (is_a($user, 'WP_User')) {
-        return $user;
-    }
-
-    if (!empty($username) && is_email($username)) {
-        $user = get_user_by('email', $username);
-        if (isset($user, $user->user_login, $user->user_status)) {
-            if (0 === intval($user->user_status)) {
-                $username = $user->user_login;
-                return wp_authenticate_username_password(null, $username, $password);
-            }
-        }
-    }
-
-    if (!empty($username) || !empty($password)) {
-        return false;
-    } else {
-        return wp_authenticate_username_password(null, "", "");
-    }
-}
-
-}
-
--- readme.txt
+++ readme.txt
@@ -1,44 +1,24 @@
 === Force Email Login ===
 Contributors: miyauchi
-Donate link: http://wpist.me/
-Tags: widget
-Requires at least: 3.4
-Tested up to: 4.0
-Stable tag: 0.4.0
+Tags: email, login
+Requires at least: 3.8
+Tested up to: 4.1
+Stable tag: 0.5.0
 
-Use email address for login to your WordPress.
+Use your email address instead of a username to log into your WordPress.
 
 == Description ==
 
-Use email address for login to your WordPress.
+Use your email address instead of a username to log into your WordPress.
 
-It is easy way to protect from brute-force attacks.
+It is an easy way to protect from brute-force attacks.
 
-[This plugin maintained on GitHub.](https://github.com/miya0001/force-email-login)
+https://github.com/miya0001/force-email-login
 
 = Some features: =
 
 * Use email address instead of the username to login.
 * Login using the username is always denied.
-* Automatically lockdown 10 seconds after login failed.
-
-= filter hooks example =
-
-You can customize lockdown time like below.
-
-`<?php
-    add_filter("force_email_login_lockdown", "my_force_email_login_lockdown");
-    function my_force_email_login_lockdown($seconds) {
-        return 60; // default 10
-    }
-?>`
-
-
-Please contact to me.
-
-* @miya0001 on twitter.
-* http://wpist.me/ (en)
-* http://firegoby.jp/ (ja)
 
 = Contributors =
 
@@ -60,4 +40,3 @@ Please contact to me.
 
 This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
 The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
-
